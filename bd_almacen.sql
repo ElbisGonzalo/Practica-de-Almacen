31702c91514a7279f0c00bcbf91f3c715b6946ea
ElbisGonzalo <65795638+ElbisGonzalo@users.noreply.github.com>
2020-06-03 04:56:00 +0200

    Add files via upload

---
+++ bd_almacen.sql
@@ -0,0 +1,23 @@
+﻿mysql -uroot -p
+
+create database bd_almacen character set utf8 collate utf8_general_ci;
+
+use bd_almacen;
+
+create table productos (
+id int(11) not null,
+descripcion varchar(250) default null,
+stock int(11) default null
+) engine=InnoDB default charset=utf8;
+
+insert into productos (`id`, `descripcion`, `stock`) VALUES(1, 'Celular Samsung J7', 105);
+insert into productos (`id`, `descripcion`, `stock`) VALUES(2, 'Laptop 15* Sony', 55);
+insert into productos (`id`, `descripcion`, `stock`) VALUES(3, 'ipod Pro 10*', 30);
+
+alter table productos
+add primary key (id);
+
+alter table productos
+modify id int(11) not null auto_increment, auto_increment=8;
+
+commit;
